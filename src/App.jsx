85bb4fd51c9f4f0eeead8929049dc654dfaf67ee
MDeadwiler <mdeadwiler@email.com>
2024-11-16 14:42:08 -0500

    finished img-started minilistingForm

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <>
       <h1 className='font-sans text-7xl'>Homi</h1>
+      
     </>
   )
 }
--- src/components/FivePicture.jsx
+++ src/components/FivePicture.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import SingleLeft from './SingleLeft';
 
 
 
@@ -14,8 +14,13 @@ import React from 'react'
 
 export const FivePicture = () => {
   return (
-    <div class="grid gap-4 grid-cols-3 grid-rows-3">
-      <img>https://a0.muscache.com/im/pictures/5e67688b-757d-44d6-8b4b-1e91dc6fe49f.jpg?im_w=1920</img>  
+    <div>
+      {/* <h1>House.location</h1> */}
+      <SingleLeft />
+      <div>
+        {/* Rani's calendar  */}
+         {/* <h2>Hosted.username</h2> */}  {/* pricing will be on the right of username */}
+        </div>
     </div>
   )
 }
