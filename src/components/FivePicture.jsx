a3542c1795ce798a2374c89099f7f3259c3498fe
MDeadwiler <mdeadwiler@email.com>
2024-11-16 13:34:59 -0500

    five-images

--- src/components/FivePicture.jsx
+++ src/components/FivePicture.jsx
@@ -14,8 +14,8 @@ import React from 'react'
 
 export const FivePicture = () => {
   return (
-    <div>
-      <img></img>  
+    <div class="grid gap-4 grid-cols-3 grid-rows-3">
+      <img>https://a0.muscache.com/im/pictures/5e67688b-757d-44d6-8b4b-1e91dc6fe49f.jpg?im_w=1920</img>  
     </div>
   )
 }
--- src/components/SingleLeft.jsx
+++ src/components/SingleLeft.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const SingleLeft = () => {
   return (
-    <div>SingleLeft</div>
+    <>
+    <div class="grid gap-4 grid-cols-3 grid-rows-2">
+        <div>
+        <img className='https://a0.muscache.com/im/pictures/5e67688b-757d-44d6-8b4b-1e91dc6fe49f.jpg?im_w=1920' alt='home1'/>
+    </div>
+    <div>
+    <img className='https://a0.muscache.com/im/pictures/5e67688b-757d-44d6-8b4b-1e91dc6fe49f.jpg?im_w=1920' alt='home1'/>
+</div>
+<div>
+        <img className='https://a0.muscache.com/im/pictures/5e67688b-757d-44d6-8b4b-1e91dc6fe49f.jpg?im_w=1920' alt='home1'/>
+    </div>
+    <div>
+    <img className='https://a0.muscache.com/im/pictures/5e67688b-757d-44d6-8b4b-1e91dc6fe49f.jpg?im_w=1920' alt='home1'/>
+</div>
+</div>
+</>
   )
 }
 
