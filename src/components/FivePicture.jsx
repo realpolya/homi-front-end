be0a35fe8365d94fd708a4e1276903aa28ceb939
mdeadwiler1 <133975362+mdeadwiler@users.noreply.github.com>
2024-11-16 13:02:27 -0500

    Md single left (#7)
    * colors for tailwind (#2)
    
    Co-authored-by: mdeadwiler1 <133975362+mdeadwiler@users.noreply.github.com>
    
    * save
    
    * added left and right for listing page
    
    * create dummy data - no photos yet (#5)
    
    Co-authored-by: mdeadwiler1 <133975362+mdeadwiler@users.noreply.github.com>
    
    * SingleLeft
    
    ---------
    
    Co-authored-by: Polina Stepanova <123517550+realpolya@users.noreply.github.com>
    Co-authored-by: MDeadwiler <mdeadwiler@email.com>

---
+++ src/components/FivePicture.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+
+
+
+
+
+
+
+
+
+
+
+export const FivePicture = () => {
+  return (
+    <div>
+      <img></img>  
+    </div>
+  )
+}
+
+export default FivePicture;
---
+++ src/components/SingleLeft.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SingleLeft = () => {
+  return (
+    <div>SingleLeft</div>
+  )
+}
+
+export default SingleLeft
