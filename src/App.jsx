d30227282418b1a08e847650cf80630be9739ba1
rbungay <64047258+rbungay@users.noreply.github.com>
2024-11-17 12:35:47 -0500

    Created Route Paths. DO NOT delete branch yet.  (#16)
    * add routes for pages
    
    * deleted readme
    
    ---------
    
    Co-authored-by: mdeadwiler1 <133975362+mdeadwiler@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 //import { useState } from 'react'
-import './App.css'
-import './index.css'
-import FivePicture from './components/FivePicture'
-import MiniListingForm from './components/MiniListingForm'
-
+import "./App.css";
+import "./index.css";
+import FivePicture from "./components/FivePicture";
+import MiniListingForm from "./components/MiniListingForm";
+import { AppRoutes } from "./Routes";
 
 function App() {
-
   return (
     <>
-      <h1 className='font-sans text-7xl'>Homi</h1>
-  
+      <h1 className="font-sans text-7xl">Homi</h1>
+      <AppRoutes />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Routes.jsx
@@ -0,0 +1,38 @@
+import { Routes, Route } from "react-router-dom";
+import { About } from "./app/About";
+import { BookingForm } from "./app/BookingForm";
+import { Bookings } from "./app/Bookings";
+import { Dashboard } from "./app/Dashboard";
+import { Landing } from "./app/Landing";
+import { Listing } from "./app/Listing";
+import { ListingForm } from "./app/ListingForm";
+import { SignIn } from "./app/Signin";
+import { SignUp } from "./app/SignUp";
+import { SingleListingBooking } from "./app/SingleListingBooking";
+
+export function AppRoutes({}) {
+  return (
+    <Routes>
+      {/* separating routes that needs authenticaiton vs public */}
+      {/* NOTE: this needs to get changed once we have services file adding {user &&()} */}
+      <>
+        <Route path="/booking-form" element={<BookingForm />} />
+        <Route path="/bookings" element={<Bookings />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/listing-form" element={<ListingForm />} />
+      </>
+      <>
+        {/*these are currently public paths*/}
+        <Route path="/" element={<Landing />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/listing" element={<Listing />} />
+        <Route
+          path="/listing/:listingId/booking"
+          element={<SingleListingBooking />}
+        />
+      </>
+    </Routes>
+  );
+}
