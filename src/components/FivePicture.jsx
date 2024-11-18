560b61c872c061b29296f937c9f0d28097d57ea9
rbungay <64047258+rbungay@users.noreply.github.com>
2024-11-18 15:03:35 -0500

    fixed css (#32)
    Co-authored-by: mdeadwiler1 <133975362+mdeadwiler@users.noreply.github.com>

--- src/components/FivePicture.jsx
+++ src/components/FivePicture.jsx
@@ -2,8 +2,7 @@ import React from "react";
 
 export const FivePicture = () => {
   return (
-    <div className="grid grid-cols-3 grid-rows-2 gap-4 rounded-lg bg-white p-4">
-      
+    <div className="grid grid-cols-3 grid-rows-2 gap-4 rounded-lg  p-4">
       <div className="row-span-2">
         <img
           src="https://a0.muscache.com/im/pictures/5e67688b-757d-44d6-8b4b-1e91dc6fe49f.jpg?im_w=1920"
@@ -12,7 +11,6 @@ export const FivePicture = () => {
         />
       </div>
 
-    
       <div>
         <img
           src="https://a0.muscache.com/im/pictures/5e67688b-757d-44d6-8b4b-1e91dc6fe49f.jpg?im_w=1920"
@@ -44,5 +42,3 @@ export const FivePicture = () => {
     </div>
   );
 };
-
-
--- src/components/SingleLeft.jsx
+++ src/components/SingleLeft.jsx
@@ -5,23 +5,21 @@ import { Calendar } from "./Calendar";
 
 export const SingleLeft = () => {
   return (
-    <div className="flex flex-col bg-white rounded-lg shadow-lg p-6 space-y-8 w-full">
+    <div className="flex flex-col p-6 space-y-8 w-full">
       {/* FivePicture Section */}
-      
+      <div className="rounded-lg p-4">
         <FivePicture />
-      
+      </div>
 
+      {/* This shows the calendar and listing form side by side */}
+      <div className="flex flex-row space-x-6  p-4 rounded-lg items-stretch">
+        {/* Calendar - 2/3 width of the container */}
+        <div className="flex w-2/3 p-4 rounded-lg">
+          <Calendar />
+        </div>
 
-
-
-
-      {/* This shows the the calendar and the listing reserve from from left to right */}
-      <div className="flex-1 columns-2 md:columns-2 bg-white p-4 rounded-lg shadow border border-teal-500">
-        <div className="flex-1 bg-white p-4 rounded-lg">
-        <Calendar />
-          </div>
-
-        <div className="flex-1 bg-white p-4 rounded-lg shadow border border-teal-500">
+        {/* Mini Listing Form - 1/3 width of the container */}
+        <div className="flex w-1/3  p-4 rounded-lg">
           <MiniListingForm />
         </div>
       </div>
