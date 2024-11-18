e9e145292672b2f5677b09d2ff1a0038ddb8c422
MDeadwiler <mdeadwiler@email.com>
2024-11-18 13:56:45 -0500

    added updated amentities

--- src/app/SingleListingBooking.jsx
+++ src/app/SingleListingBooking.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 import { SingleLeft } from "../components/SingleLeft";
 import { SingleRight } from "../components/SingleRight";
+import {Navbar} from "../components/NavBar";
+
 
 export const SingleListingBooking = () => {
   return (
-    
     <>
-      <h1 className="fixed top-0 left-0 font-sans text-7xl p-4">homi</h1>
       
+      <Navbar />
       {/* Left Section */}
       <div className="flex-col md:flex-row">
-      <div className="flex-1 bg-white rounded-lg  p-6">
-        <SingleLeft />
+        <div className="flex-1 bg-white rounded-lg  p-6">
+          <SingleLeft />
+        </div>
+
+        {/* Right Section */}
+        <div className="flex-1 h-lvh bg-white rounded-lg  p-6">
+          <SingleRight />
+        </div>
       </div>
-      
-      {/* Right Section */}
-      <div className="flex-1 h-lvh bg-white rounded-lg  p-6">
-        <SingleRight />
-      </div>
-      </div>
-      </>
+    </>
   );
 };
-
