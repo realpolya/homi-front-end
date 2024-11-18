20af900cf05add74273e4993caa72c2f2f31c4f8
rbungay <64047258+rbungay@users.noreply.github.com>
2024-11-18 17:02:11 -0500

    update css stylings on bookinglisting (#37)
    * fixed css
    
    * update css and removed unnecessary divs
    
    * update css
    
    ---------
    
    Co-authored-by: mdeadwiler1 <133975362+mdeadwiler@users.noreply.github.com>

--- src/components/SingleRight.jsx
+++ src/components/SingleRight.jsx
@@ -2,34 +2,15 @@ import React from "react";
 import { Amenities } from "./Amenities";
 import { ListingDescription } from "./ListingDescription";
 
-export const SingleRight = ({ property }) => {
+export const SingleRight = () => {
   return (
-    <div className="flex flex-col bg-white rounded-lg shadow-lg p-6 space-y-8 w-full">
-      {/* I just needed this here to get a full layout of my component. Polina's mao will replace this*/}
-      <div className="relative bg-green-200 h-48 rounded-lg flex items-center justify-center border-2 border-blue-300 mb-8">
-        <p className="text-gray-600">Map Placeholder</p>
-      </div>
-
-      
-      
-      
-      
-      {/* This shows the bottom for  Amenities on Left, ListingDescription on the Right */}
-      <div className="flex-1 columns-2 md:columns-2 bg-white p-4 rounded-lg shadow border border-teal-500">
-        <div className="flex-1 bg-white p-4 rounded-lg">
-        
-          {/* Left Side: Amenities */}
-        
-        
-        
+    <div className="flex flex-col p-6 space-y-8 w-full w-1/2 bg-whiteColor p-6 rounded-lg min-h-screen">
+      THIS IS FOR THE MAP
+      {/* This shows the calendar and listing form side by side */}
+      <div className="flex flex-row space-x-6  p-4 rounded-lg items-stretch">
         <Amenities />
-
-        {/* Right Side: Listing Description */}
-        <div className="flex-1 bg-white p-4 rounded-lg shadow border border-teal-500">
-        <ListingDescription property={property} />
-        </div>
+        <ListingDescription />
       </div>
     </div>
-   </div>
   );
 };
