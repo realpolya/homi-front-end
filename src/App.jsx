2de7dc3eeedce4dad688399251f17813f8784c1a
MDeadwiler <mdeadwiler@email.com>
2024-11-18 14:46:34 -0500

    almost done with navbar

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { AppRoutes } from "./Routes";
 function App() {
   return (
     <>
-      <h1 className="font-sans text-7xl">Homi</h1>
+    
       <AppRoutes />
     </>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
+import { FaBars, FaMapMarkedAlt } from "react-icons/fa";
 
 export const NavBar = () => {
   return (
-    <div>NavBar homi </div>
-  )
-}
+    <nav className="px-4 py-3 flex justify-center">
+      <div className="flex items-center text-xl">
+        <FaBars className=" text-textColor me-4 cursor-pointer" />
+        <span className="text-textColor">Where:</span>
+      </div>
+      <div className="flex items-center gap-x-5">
+        <div className="relative md:w-65">
+          <span className="relative md:absolute inset-y-0 left-0 flex items-center pl-2">
+            <button className="p-1 focus:outline-none text-textColor md:text-black"><FaMapMarkedAlt /></button></span>
+          <input type="text" />
+        </div>
+      </div>
+    </nav>
+  );
+};
