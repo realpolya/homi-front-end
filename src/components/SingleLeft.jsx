c24c4e77af9d6ae7d7406ac9106caa271152cd8c
realpolya <ps2780@columbia.edu>
2024-11-19 17:59:00 -0500

    work on logic for single listing booking, then work on five picture

--- src/components/SingleLeft.jsx
+++ src/components/SingleLeft.jsx
@@ -1,17 +1,21 @@
-import React from "react";
 import { FivePicture } from "./FivePicture";
 import { MiniListingForm } from "./MiniListingForm";
 import { Calendar } from "./Calendar";
 
 export const SingleLeft = () => {
+
   return (
     <div className="flex flex-col p-6 space-y-8 w-full w-1/2 bg-whiteColor p-6 rounded-lg min-h-screen">
+      
       <FivePicture />
+
       {/* This shows the calendar and listing form side by side */}
       <div className="flex flex-row space-x-6  p-4 rounded-lg items-stretch">
         <Calendar />
         <MiniListingForm />
       </div>
+
     </div>
   );
+
 };
