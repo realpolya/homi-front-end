04413b86341f25c2ce701b08a527b9f2ee59c7c8
MDeadwiler <mdeadwiler@email.com>
2024-11-19 18:34:59 -0500

    doing global variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,84 @@
-import { useState, useEffect } from 'react';
-import Rodal from 'rodal';
-import 'rodal/lib/rodal.css';
-import './App.css';
-import './index.css';
-import { AppRoutes } from './Routes';
-import { SignUp } from './app/SignUp';
-import { SignIn } from './app/Signin';
-import { Navbar } from './components/NavBar';
-import { Footer } from './components/Footer';
-import { getUser } from './services/sub_services/userServices';
+import { useState, useEffect } from "react";
+import Rodal from "rodal";
+import "rodal/lib/rodal.css";
+import "./App.css";
+import "./index.css";
+import { AppRoutes } from "./Routes";
+import { SignUp } from "./app/SignUp";
+import { SignIn } from "./app/Signin";
+import { Navbar } from "./components/NavBar";
+import { Footer } from "./components/Footer";
+import { getUser } from "./services/sub_services/userServices";
+import { Listings } from "./app/Listings";
+import { Bookings } from "./app/Bookings";
+import { Amenities } from "./components/Amenities";
+
+
+
+
 
 function App() {
-  const [user, setUser] = useState(null)
   const [activeModal, setActiveModal] = useState(null);
+  const [user, setUser] = useState(null);
+  const [listings, setListings] = useState([]);
+  const [bookings, setBookings] = useState([]);
+  const [amenities, setAmenities] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     const fetchUser = async () => {
-      const userData = await getUser()
-      setUser(userData)
-    }
+      try {
+        const userData = await getUser();
+        setUser(userData);
+      } catch (err) {
+        console.error("Failed to get user data", err);
+      }
+    };
+    fetchUser();
+  }, []);
 
-    fetchUser()
-  }, [])
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        const listingsData = await listings();
+        setListings(listingsData);
+      } catch (err) {
+        console.error("Failed to get user data", err);
+      }
+    };
+    fetchListings();
+  }, []);
+
+  useEffect(() => {
+    const fetchBookings = async () => {
+      try {
+        const bookingsData = await bookings();
+        setBookings(bookingsData);
+      } catch (err) {
+        console.error("Failed to get user data", err);
+      }
+    };
+    fetchBookings();
+  }, []);
+
+  useEffect(() => {
+    const fetchAmenities = async () => {
+      try {
+        const amenitiesData = await amenities();
+        setAmenities(amenitiesData);
+      } catch (err) {
+        console.error("Failed to get user data", err);
+      }
+    };
+    fetchAmenities();
+  }, []);
 
   const handleSignUp = (data) => {
-    setUser(data)
+    setUser(data);
     setActiveModal(null);
   };
 
   const handleSignIn = (data) => {
-    setUser(data)
+    setUser(data);
     setActiveModal(null);
   };
 
