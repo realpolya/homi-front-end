e2965e028386fb51f2f4d8bd70cc4074ed261a43
realpolya <ps2780@columbia.edu>
2024-11-19 14:59:08 -0500

    write to do list for property services

--- src/services/sub_services/propertyServices.js
+++ src/services/sub_services/propertyServices.js
@@ -6,24 +6,46 @@ import api from './apiConfig.js';
 
 const getProperties = async (query=null) => {
     try {
-        console.log('inside get properties')
+
         let response = undefined;
+
+        // passing queries in url and into the function
         if (query) {
+
             console.log('services: query for getProperties', query)
             response = await api.get(`properties/?${query}`);
+        
+        // general retrieval of all properties
         } else {
+
             console.log('services: no query for getProperties')
             response = await api.get('properties/');
+
         }
-        console.log(response)
+
+        console.log(response.data)
         return response.data
+
     } catch (err) {
+
         console.log(err.response.data.error);
         throw err
+
     }
 }
 
-// TODO: 
+// TODO: postProperty
+
+// TODO: getMyProperties
+
+// TODO: getMyArchived
+
+// TODO: getUserProperties
+
+// TODO: getSingleProperty
+
+// TODO: putProperty (achiving properties is included here)
+
 
 /* --------------------------------Exports--------------------------------*/
 
