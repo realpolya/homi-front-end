71bd872fcc60352fd665af60f0c002cc42cc1868
realpolya <ps2780@columbia.edu>
2024-11-19 15:24:42 -0500

    create to do list for booking services

--- src/services/sub_services/bookingServices.js
+++ src/services/sub_services/bookingServices.js
@@ -4,12 +4,29 @@ import api from './apiConfig.js';
 
 /* --------------------------------Functions--------------------------------*/
 
+// TODO: get persons's bookings
 const getBookings = () => {
+
+    
 
     console.log('getting bookings')
 
 }
 
+
+// TODO: view upcoming bookings
+
+// TODO: create a new booking
+
+// TODO: retrieve one booking
+
+// TODO: update one booking
+
+// TODO: delete one booking
+
+// TODO: host's bookings
+
+// TODO: property's bookings 
 
 /* --------------------------------Exports--------------------------------*/
 
