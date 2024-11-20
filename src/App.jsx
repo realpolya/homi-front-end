ebb9b1a75e6ecafb426e80dd6f90631d1741780a
MDeadwiler <mdeadwiler@email.com>
2024-11-19 21:55:38 -0500

    signout gone

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,14 @@ import { SignUp } from "./app/SignUp";
 import { SignIn } from "./app/Signin";
 import { Navbar } from "./components/NavBar";
 import { Footer } from "./components/Footer";
-import { getUser } from "./services/sub_services/userServices";
-// import { Listings } from "./app/Listings";
-// import { Bookings } from "./app/Bookings";
-// import { Amenities } from "./components/Amenities";
-
-
-
+import { getUser} from "./services/sub_services/userServices";
 
 
 function App() {
   const [activeModal, setActiveModal] = useState(null);
   const [user, setUser] = useState(null);
-  // const [listings, setListings] = useState([]);
-  // const [bookings, setBookings] = useState([]);
-  // const [amenities, setAmenities] = useState([]);
+  
+
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -36,42 +29,9 @@ function App() {
     fetchUser();
   }, []);
 
-  // useEffect(() => {
-  //   const fetchListings = async () => {
-  //     try {
-  //       const listingsData = await listings();
-  //       setListings(listingsData);
-  //     } catch (err) {
-  //       console.error("Failed to get listings", err);
-  //     }
-  //   };
-  //   fetchListings();
-  // }, []);
+  
 
-  // useEffect(() => {
-  //   const fetchBookings = async () => {
-  //     try {
-  //       const bookingsData = await bookings();
-  //       setBookings(bookingsData);
-  //     } catch (err) {
-  //       console.error("Failed to get bookings", err);
-  //     }
-  //   };
-  //   fetchBookings();
-  // }, []);
-
-  // useEffect(() => {
-  //   const fetchAmenities = async () => {
-  //     try {
-  //       const amenitiesData = await amenities();
-  //       setAmenities(amenitiesData);
-  //     } catch (err) {
-  //       console.error("Failed to get amenities", err);
-  //     }
-  //   };
-  //   fetchAmenities();
-  // }, []);
-
+  
   const handleSignUp = (data) => {
     setUser(data);
     setActiveModal(null);
@@ -82,70 +42,31 @@ function App() {
     setActiveModal(null);
   };
 
-  const handleSignOut = (data) => {
-    setUser(data)
-    setActiveModal(null)
-  }
-
   return (
-    <>
+   <>
       <Navbar
         setShowRegister={() => setActiveModal("register")}
         setShowLogin={() => setActiveModal("login")}
-        setShowSignOut={() => setActiveModal("signOut")}
         user={user}
       />
       <AppRoutes />
-
-      {/* Register Modal */}
+      {/* Modals */}
       <Rodal
         visible={activeModal === "register"}
         onClose={() => setActiveModal(null)}
-        closeOnEsc={true}
-        closeMaskOnClick={true}
-        customStyles={{
-          width: "400px",
-          height: "400px",
-          padding: "20px",
-          borderRadius: "10px",
-        }}
+        customStyles={{ width: "400px", height: "400px", borderRadius: "10px" }}
       >
         <SignUp onSubmit={handleSignUp} />
       </Rodal>
-
-      {/* Login Modal */}
       <Rodal
         visible={activeModal === "login"}
         onClose={() => setActiveModal(null)}
-        closeOnEsc={true}
-        closeMaskOnClick={true}
-        customStyles={{
-          width: "400px",
-          height: "350px",
-          padding: "20px",
-          borderRadius: "10px",
-        }}
+        customStyles={{ width: "400px", height: "350px", borderRadius: "10px" }}
       >
         <SignIn onSubmit={handleSignIn} />
       </Rodal>
-    {/* signout waiting to be added */} 
-       <Rodal
-        visible={activeModal === "signout"}
-        onClose={() => setActiveModal(null)}
-        closeOnEsc={true}
-        closeMaskOnClick={true}
-        customStyles={{
-          width: "400px",
-          height: "350px",
-          padding: "20px",
-          borderRadius: "10px",
-        }}
-      >
-      <Signout onSubmit={handleSignOut} />
-      </Rodal>
-
       <Footer />
-    </>
+      </>
   );
 }
 
