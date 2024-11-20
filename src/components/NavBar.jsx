b9661a86c93dcab478254a9e0ed6a72aac695f1e
zjyezaire <zaire.elleby@gmail.com>
2024-11-20 13:02:29 -0500

    create property form functionality

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Twirl as Hamburger } from "hamburger-react";
 import { FaSearch } from "react-icons/fa";
 import { Sidebar } from "./Sidebar";
@@ -6,9 +6,33 @@ import { NavLink } from "react-router-dom";
 
 export const Navbar = ({ setShowRegister, setShowLogin, user }) => {
   const [isOpen, setOpen] = useState(false);
+  const navbarRef = useRef(null);
+
+  // Close the sidebar when clicking outside
+  const handleClickOutside = (event) => {
+    if (navbarRef.current && !navbarRef.current.contains(event.target)) {
+      setOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    // Cleanup the event listener on unmount
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpen]);
 
   return (
-    <nav className="flex justify-evenly items-center w-full fixed top-0 left-0 bg-backgroundColor  z-10 px-4 py-2">
+    <nav
+      ref={navbarRef}
+      className="flex justify-evenly items-center w-full fixed top-0 left-0 bg-backgroundColor z-10 px-4 py-2"
+    >
       <NavLink to="/" className="text-4xl">
         homi
       </NavLink>
@@ -23,7 +47,6 @@ export const Navbar = ({ setShowRegister, setShowLogin, user }) => {
           <FaSearch />
         </button>
       </div>
-      {/* will design the the open navbar */}
       <Hamburger toggled={isOpen} toggle={setOpen} />
       {isOpen && (
         <Sidebar
