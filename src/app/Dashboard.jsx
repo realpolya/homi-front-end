78180c27693408b8bdf6025f3871331dbd55ec02
realpolya <ps2780@columbia.edu>
2024-11-21 09:29:55 -0500

    add conditional rendering to single left

--- src/app/Dashboard.jsx
+++ src/app/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import { AppContext } from '../App.jsx'
 import { useLocation } from "react-router-dom";
 import { MyUserInfo } from "../components/MyUserInfo";
