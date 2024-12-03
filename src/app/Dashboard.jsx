ed7cf99c24ae0b3ce5cc53cc0ea7d0a691fcbe2b
realpolya <ps2780@columbia.edu>
2024-12-03 17:29:54 -0500

    rearrange dashboard

--- src/app/Dashboard.jsx
+++ src/app/Dashboard.jsx
@@ -9,101 +9,96 @@ import { HostBookings } from "../components/MyBookingsListings"
 import { BookingMap } from "../components/BookingMap"
 import { ListingCard } from "../components/ListingCard"
 
-import { getUpcoming, getMyProperties, getHostBookings } from "../services"
+import services from "../services/index.js"
 
 /* --------------------------------Component--------------------------------*/
 
 const Dashboard = () => {
 
-    const location = useLocation();
-    const isHost = location.pathname.includes("host")
+    const { bookings } = useContext(AppContext)
+
+    const location = useLocation()
+
+    const [isHost, setIsHost] = useState(false)
     const [upcomingBookings, setUpcomingBookings] = useState([]);
     const [myProperties, setMyProperties] = useState([]);
     const [hostBookings, setHostBookings] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState("");
-
     const [prevBookings, setPrevBookings] = useState([])
 
-    const { bookings } = useContext(AppContext)
 
-
-
-    // Fetch upcoming bookings when the component mounts
-    useEffect(() => {
-    if (!isHost) {
-        // Only fetch upcoming bookings if the user is a guest (not a host)
-        const fetchUpcomingBookings = async () => {
+    const fetchUpcomingBookings = async () => {
         try {
-            const bookings = await getUpcoming();
-            setUpcomingBookings(bookings);
+            setUpcomingBookings(await services.getUpcoming())
         } catch (error) {
-            console.error("Error fetching upcoming bookings:", error);
+            console.error("Error fetching upcoming bookings:", error)
         }
-        };
-
-        fetchUpcomingBookings();
     }
-    }, [isHost]); // Re-run when `isHost` changes
 
-
-    useEffect(() => {
-    if (isHost) {
-        const fetchHostData = async () => {
+    const fetchHostData = async () => {
         try {
-            const [properties, bookings] = await Promise.all([
-            getMyProperties(),
-            getHostBookings(),
-            ]);
 
-            console.log("Host Properties:", properties);
-            console.log("Host Bookings:", bookings);
+            const properties = await services.getMyProperties()
 
             setMyProperties(
-            properties.map((prop) => ({
-                id: prop.id,
-                prop,
-            }))
+                properties.map((prop) => ({
+                    id: prop.id,
+                    prop,
+                }))
             );
-            setHostBookings(bookings);
+
+            setHostBookings(await services.getHostBookings());
+
         } catch (error) {
             console.error("Error fetching host data:", error);
             setError("Failed to load host data. Please try again.");
         } finally {
             setLoading(false);
         }
-        };
+    };
 
-        fetchHostData();
-    }
-    }, [isHost]);
+    useEffect(() => {
+
+        if (location.pathname.includes("host")) setIsHost(true)
+
+    }, [location.pathname])
 
 
     useEffect(() => {
 
-    if (bookings) {
+        if (!isHost) fetchUpcomingBookings()
 
-        let prevData = []
-        bookings.forEach(booking => {
+        if (isHost) fetchHostData()
 
-        if (booking.prop) {
-            if (booking.prop.address) {
-            let prevInstance = {
-                latitude: booking.prop.address.latitude,
-                longitude: booking.prop.address.longitude,
-                title: booking.prop.title,
-                check_in: booking.check_in_date,
-                check_out: booking.check_out_date
-            }
-            prevData.push(prevInstance)
-            }
+    }, [isHost])
+
+
+    useEffect(() => {
+
+        if (bookings) {
+
+            let prevData = []
+            bookings.forEach(booking => {
+
+                if (booking.prop) {
+                    if (booking.prop.address) {
+                    let prevInstance = {
+                        latitude: booking.prop.address.latitude,
+                        longitude: booking.prop.address.longitude,
+                        title: booking.prop.title,
+                        check_in: booking.check_in_date,
+                        check_out: booking.check_out_date
+                    }
+                    prevData.push(prevInstance)
+                    }
+                }
+
+            })
+
+            setPrevBookings(prevData);
+
         }
-
-        })
-
-        setPrevBookings(prevData);
-
-    }
 
     }, [bookings])
 
@@ -136,11 +131,12 @@ const Dashboard = () => {
             <p className="text-lightTextColor">Your Upcoming Bookings</p>
             <div className="flex flex-wrap gap-4">
                 {upcomingBookings.length > 0 ? (
-                upcomingBookings.map((booking) => {
+                upcomingBookings.map((booking, i) => {
                     return <ListingCard 
                     bookingId={booking.id} 
                     listing={booking.prop} 
                     origin={"dashboard"}
+                    key={i}
                     booking={booking} />
                 })
                 ) : (
